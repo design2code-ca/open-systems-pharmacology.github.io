d87dbce3f09a1f0f1b3fb83d31e50b09980e64cc
rodolfo <rodolfo@design2code.ca>
2019-04-26 16:14:35 -0400

    * Responsive carousel

--- js/site.js
+++ js/site.js
@@ -38,6 +38,31 @@ $(document).ready(function() {
   $(".team-container").slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '125px'
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
